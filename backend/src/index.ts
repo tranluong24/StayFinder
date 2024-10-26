fbe55cf201d40e44113c9a43afe0b62c9b999e25
luongtr-ndev <hungngtk123@gmail.com>
2024-10-26 23:59:16 +0700

    Connect MongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,28 @@
 import express, {Request, Response} from 'express'
 import cors from 'cors'
 import "dotenv/config"
+import mongose from 'mongoose'
 
-const app = express();
+mongose
+.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then(()=>{
+    console.log('Connected MongoDB')
+})
+.catch((err) =>{
+    console.log('Error: ', err)
+})
 
+const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
+
+const PORT = 3001
 
 app.get("/api/test", async(req: Request, res: Response)=>{
     res.json({message:"Hello from Express !"})
 })
 
-app.listen(3001, ()=>{
+app.listen(PORT, ()=>{
     console.log("Server running on localhost:3001")
 })
