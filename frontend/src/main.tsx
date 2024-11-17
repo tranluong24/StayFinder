bcf62c32f2673309199312026a809db44891ee4f
PhanBaoLong2311 <long855989@gmail.com>
2024-11-18 00:51:26 +0700

    Them form dang ky tai khoan

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { AppContextProvider } from './contexts/AppContexts.tsx'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </QueryClientProvider>
+  </StrictMode>
 )
