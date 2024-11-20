fe2140affe7129f452c1d150ae8ffa383b9d01f8
LuongTran <luongtran1312003@gmail.com>
2024-11-20 19:52:44 +0700

    create hotel form

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -10,6 +10,7 @@ type ToastMessage = {
 
 type AppContext = {
     showToast: (toastMessage: ToastMessage) => void;
+    isLoggedIn: boolean;
 }
 
 const AppContext = React.createContext<AppContext | undefined>(undefined);
