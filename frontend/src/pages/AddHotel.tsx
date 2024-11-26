6c8af3f4fde8c0474d66373ed378d5f05615e6f9
PhanBaoLong2311 <long855989@gmail.com>
2024-11-26 23:37:30 +0700

    Them giao dien manager hotel form - Chon so luong khach - Chon cac tieu chi cho khach san - Them anh cho khach san - Viet automated test cho manager hotel

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,7 +1,25 @@
+import { useMutation } from "react-query";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
+import * as apiClient from "../api-client"
 
 const AddHotel = () => {
-    return <ManageHotelForm />;
+    const {showToast} = useAppContext();
+
+    const { mutate, isLoading} = useMutation(apiClient.addMyHotel, {
+        onSuccess: ()=>{
+            showToast({ message: "Hotel Saved!", type: "SUCCESS"})
+        },
+        onError: ()=>{
+            showToast({ message: "Error Saving Hotel", type: "ERROR"})
+        }
+    })
+
+    const handelSave = (hotelFormData: FormData) => {
+        mutate(hotelFormData)
+    }
+
+    return <ManageHotelForm onSave={handelSave} isLoading={isLoading} />;
 };
 
 export default AddHotel;
