ac293058832509799490f384e1f325500322b7b7
LuongTran <luongtran1312003@gmail.com>
2024-11-29 23:44:10 +0700

    Add view my hotel feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Register from './pages/Register'
 import SignIn from './pages/SignIn'
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
 
 
 const App = () => {
@@ -44,6 +45,13 @@ const App = () => {
               path="/add-hotel" element = {
                 <Layout>
                   <AddHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-hotels" element = {
+                <Layout>
+                  <MyHotels />
                 </Layout>
               }
             />
