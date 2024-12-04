d2d55ec5f481f51944eaa6c587a77fd9bbd9df6c
luongtr-ndev <hungngtk123@gmail.com>
2024-12-04 13:15:45 +0700

    add feature detail in hotel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
+import Detail from "./pages/Detail";
 
 
 const App = () => {
