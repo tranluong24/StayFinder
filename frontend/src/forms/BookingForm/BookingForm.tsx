df877995197c4f0fc56ca4dbf9acfe57e05e72dd
LuongTran <luongtran1312003@gmail.com>
2024-12-05 10:40:17 +0700

    booking feature

--- frontend/src/forms/BookingForm/BookingForm.tsx
+++ frontend/src/forms/BookingForm/BookingForm.tsx
@@ -1,27 +1,87 @@
 import { useForm } from "react-hook-form";
-import { UserType } from "../../../../backend/src/shared/types";
+import { PaymentIntentResponse, UserType } from "../../../../backend/src/shared/types";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useSearchContext } from "../../contexts/SearchContext";
+import { useParams } from "react-router-dom";
+import { useMutation } from "react-query";
+import * as apiClient from "../../api-client";
+import { StripeCardElement } from "@stripe/stripe-js";
+import { useAppContext } from "../../contexts/AppContext";
 
 type Props = {
     currentUser: UserType;
+    paymentIntent: PaymentIntentResponse;
 }
 
-type BookingFormData = {
-    firstName: string,
-    lastName: string,
-    email: String,
+ export type BookingFormData = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    adultCount: number;
+    childCount: number;
+    checkIn: string;
+    checkOut: string;
+    hotelId: string;
+    paymentIntentId: string;
+    totalCost: number;
 }
 
-const BookingForm = ({ currentUser }: Props) => {
+const BookingForm = ({ currentUser, paymentIntent }: Props) => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const search = useSearchContext();
+    const { hotelId } = useParams();
+
+    const { showToast } = useAppContext();
+
+    const { mutate: bookRoom, isLoading } = useMutation(
+        apiClient.createRoomBooking,
+        {
+          onSuccess: () => {
+            showToast({ message: "Booking Saved!", type: "SUCCESS" });
+          },
+          onError: () => {
+            showToast({ message: "Error saving booking", type: "ERROR" });
+          },
+        }
+      );
+
     const { handleSubmit, register } = useForm<BookingFormData>({
         defaultValues: {
             firstName: currentUser.firstName,
             lastName: currentUser.lastName,
             email: currentUser.email,
+            adultCount: search.adultCount,
+            childCount: search.childCount,
+            checkIn: search.checkIn.toISOString(),
+            checkOut: search.checkOut.toISOString(),
+            hotelId: hotelId,
+            totalCost: paymentIntent.totalCost,
+            paymentIntentId: paymentIntent.paymentIntentId,
         }
     })
 
+    const onSubmit = async (formData: BookingFormData) => {
+        if (!stripe || !elements) {
+          return;
+        }
+    
+        const result = await stripe.confirmCardPayment(paymentIntent.clientSecret, {
+          payment_method: {
+            card: elements.getElement(CardElement) as StripeCardElement,
+          },
+        });
+    
+        if (result.paymentIntent?.status === "succeeded") {
+          bookRoom({ ...formData, paymentIntentId: result.paymentIntent.id });
+        }
+      };
+
     return (
-        <form className="grid grid-cols-1 gap-5 rounded-lg border border-slate-300 p-5">
+        <form 
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid grid-cols-1 gap-5 rounded-lg border border-slate-300 p-5">
             <span className="text-3xl font-bold">
                 Confirm Your Details
             </span>
@@ -54,6 +114,36 @@ const BookingForm = ({ currentUser }: Props) => {
                     />
                 </label>
             </div>
+
+            <div className="space-y-2">
+                <h2 className="text-xl font-semibold">Your Price Summary</h2>
+
+                <div className="bg-blue-200 p-4 rounded-md">
+                    <div className="font-semibold text-lg">
+                    Total Cost: £{paymentIntent.totalCost.toFixed(2)}
+                    </div>
+                    <div className="text-xs">Includes taxes and charges</div>
+                </div>
+            </div>
+
+            <div className="space-y-2">
+                <h3 className="text-xl font-semibold"> Payment Details</h3>
+                <CardElement
+                id="payment-element"
+                className="border rounded-md p-2 text-sm"
+                />
+            </div>
+
+            <div className="flex justify-end">
+                <button
+                disabled={isLoading}
+                type="submit"
+                className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-md disabled:bg-gray-500"
+                >
+                {isLoading ? "Saving..." : "Confirm Booking"}
+                </button>
+            </div>
+
         </form>
     )
 }
--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -5,8 +5,11 @@ import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import BookingDetailsSummary from "../components/BookingDetailsSummary"
 import { useSearchContext } from "../contexts/SearchContext"
+import { useAppContext } from "../contexts/AppContext"
+import { Elements } from "@stripe/react-stripe-js"
 
 const Booking = () => {
+    const { stripePromise } = useAppContext();
     const search = useSearchContext()
     const { hotelId } = useParams()
 
@@ -20,6 +23,18 @@ const Booking = () => {
             setNumberOfNights(Math.ceil(nights))
         }
     }, [search.checkIn, search.checkOut])
+
+    const { data: paymentIntentData } = useQuery(
+        "createPaymentIntent",
+        () =>
+          apiClient.createPaymentIntent(
+            hotelId as string,
+            numberOfNights.toString()
+          ),
+        {
+          enabled: !!hotelId && numberOfNights > 0,
+        }
+      );
 
     const { data: hotel } = useQuery("fetchHotelByID", () =>
         apiClient.fetchMyHotelById(hotelId as string), {
@@ -36,17 +51,31 @@ const Booking = () => {
         return <></>
     }
 
-    return <div className="grid md:grid-cols-[]">
-        <BookingDetailsSummary
+    return (
+        <div className="grid md:grid-cols-[1fr_2fr]">
+          <BookingDetailsSummary
             checkIn={search.checkIn}
             checkOut={search.checkOut}
             adultCount={search.adultCount}
-            childCount={search.childCount} 
+            childCount={search.childCount}
             numberOfNights={numberOfNights}
             hotel={hotel}
-        />
-        {currentUser && <BookingForm currentUser={currentUser} />}
-    </div>
+          />
+          {currentUser && paymentIntentData && (
+            <Elements
+              stripe={stripePromise}
+              options={{
+                clientSecret: paymentIntentData.clientSecret,
+              }}
+            >
+              <BookingForm
+                currentUser={currentUser}
+                paymentIntent={paymentIntentData}
+              />
+            </Elements>
+          )}
+        </div>
+    );
 }
 
 export default Booking
